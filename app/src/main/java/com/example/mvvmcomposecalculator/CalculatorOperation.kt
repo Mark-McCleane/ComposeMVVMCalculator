623d804634c0158b105a869ffd10490692d43e7e
Mark-McCleane <mccleanemark@gmail.com>
2022-06-25 20:40:41 +0100

    Create calculator

---
+++ app/src/main/java/com/example/mvvmcomposecalculator/CalculatorOperation.kt
@@ -0,0 +1,10 @@
+package com.example.mvvmcomposecalculator
+
+sealed class CalculatorOperation(val symbol: String){
+    object Add: CalculatorOperation("+")
+    object Subtract: CalculatorOperation("-")
+    object Multiply: CalculatorOperation("X")
+    object Divide: CalculatorOperation("/")
+
+
+}
